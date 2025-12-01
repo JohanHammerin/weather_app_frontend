c1a25f80f12d5f578b5aea0f422328eee8988b58
amandalyckenius <alyckenius@gmail.com>
2025-12-01 11:34:40 +0100

    feat(auth): create authservice and fetch /login and /logout

---
+++ app/service/authService.ts
@@ -0,0 +1,25 @@
+const AUTH_BASE_URL = process.env.NEXT_PUBLIC_AUTH_URL!;
+
+export async function login(payload: {username: string, password: string}) {
+
+    const response = await fetch (`${AUTH_BASE_URL}/auth/login`,{
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        credentials: "include",
+        body: JSON.stringify(payload),
+    })
+
+    if (!response.ok) throw new Error ("Login failed");
+    return response.json().catch(()=>null);
+
+}
+
+export async function logout(){
+    const response = await fetch(`${AUTH_BASE_URL}/auth/logout`, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        credentials: "include",
+    })
+
+    if(!response.ok) throw new Error("Logout failed")
+}
