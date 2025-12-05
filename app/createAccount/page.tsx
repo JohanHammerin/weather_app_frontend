31f55a611980aed17f78c5381de75e0ab868ed9c
amandalyckenius <alyckenius@gmail.com>
2025-12-05 14:09:29 +0100

    fix: email instead of username in email placeholder

--- app/createAccount/page.tsx
+++ app/createAccount/page.tsx
@@ -64,7 +64,7 @@ export default function Page() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter username here"
+                placeholder="Enter email here"
                 required
               />
             </section>
