77f75d99337cd4c881b0ac01bc4e73507864981e
amandalyckenius <alyckenius@gmail.com>
2025-12-05 15:50:15 +0100

    fix: empty message in handleSubmit
    change type from username to text and add autoComplete

--- app/createAccount/page.tsx
+++ app/createAccount/page.tsx
@@ -17,9 +17,9 @@ export default function Page() {
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
     
-    setIsLoading(true)
-
     e.preventDefault();
+    setIsLoading(true)
+    setMessage("");
 
     try {
       await register({username, email, password });
@@ -53,8 +53,9 @@ export default function Page() {
                 Username
               </label>
               <input
-                type="username"
+                type="text"
                 id="username"
+                autoComplete="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
